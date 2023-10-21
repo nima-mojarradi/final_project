98616d9860dc8f9a4fba707d6d8775cdd2e586bb
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-10-21 17:04:37 +0330

    feat:new models registered

--- podcast_manager/rss_parser/admin.py
+++ podcast_manager/rss_parser/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RSSLink, Comment, PodcastData, LikeEpisode, LikePodcast, EpisodeData
+from .models import RSSLink, Comment, PodcastData, LikeEpisode, LikePodcast, EpisodeData, Recommendation, Subscription, BookMark, Notification
 
 admin.site.register(RSSLink)
 admin.site.register(LikePodcast)
@@ -7,3 +7,10 @@ admin.site.register(LikeEpisode)
 admin.site.register(PodcastData)
 admin.site.register(EpisodeData)
 admin.site.register(Comment)
+admin.site.register(Recommendation)
+admin.site.register(Subscription)
+admin.site.register(BookMark)
+admin.site.register(Notification)
+
+
+
