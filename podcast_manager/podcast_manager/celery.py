f0bbf2c4894094257894ee4eaf70d5cf46cea209
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-10-21 17:02:59 +0330

    fix:time for running celery tasks corrected

--- podcast_manager/podcast_manager/celery.py
+++ podcast_manager/podcast_manager/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from celery.schedules import crontab
-
+import logging
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'podcast_manager.settings')
 
@@ -12,6 +12,7 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
 
+logging.getLogger("celery").addHandler(logging.FileHandler(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../logs/celery.log')))
 
 app.conf.beat_schedule = {
     'podcast-parse-task': {
