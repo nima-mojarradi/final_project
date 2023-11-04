2f00792349f61f5660bfb3c90df7bd9438e426eb
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-11-04 23:30:08 +0330

    feat:handler for elastic logging added

---
+++ podcast_manager/rss_parser/handler.py
@@ -0,0 +1,35 @@
+import json
+import logging
+import time
+from datetime import datetime
+from podcast_manager import settings
+from elasticsearch import Elasticsearch
+import pytz
+
+tz = pytz.timezone(settings.TIME_ZONE)
+
+class ElasticHandler(logging.Handler):
+
+    def __init__(self):
+        super().__init__()
+        self.es = Elasticsearch(f'http://elastic:9200')
+        self.sender = LogSender(self.es)
+
+    def emit(self, record):
+        try:
+            self.sender.writeLog(record, formatter=self.format)
+        except Exception:
+            self.handleError(record)
+
+
+class LogSender:
+    def __init__(self, es):
+        self.es = es
+
+    def writeLog(self, msg: logging.LogRecord, formatter):
+        index_name = f'log_date{time.strftime("%Y_%m_%d")}'
+        timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        log_data = json.loads(formatter(msg))
+        log_data['timestamp'] = timestamp
+
+        self.es.index(index=index_name, document=log_data)
