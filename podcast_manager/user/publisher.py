7ccbae09154adcf1ea309496b4b97b25f5a25332
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-11-05 18:16:09 +0330

    feat:hearbeat for connection added

--- podcast_manager/user/publisher.py
+++ podcast_manager/user/publisher.py
@@ -1,17 +1,18 @@
 import pika
 import time
 import os
+import json
 # from podcast_manager.settings import ALLOWED_HOSTS
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'podcast_manager.settings')
 # credintial = pika.PlainCredentials(username='guest', password='guest')
 
 def publisher(queue, body):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', heartbeat=60))
     channel = connection.channel()
 
     channel.queue_declare(queue=queue)
-    channel.basic_publish(exchange="", routing_key=queue, body=body)
+    channel.basic_publish(exchange="", routing_key=queue, body=json.dumps(body))
     print('finished'+'*'*100)
 
     connection.close()
